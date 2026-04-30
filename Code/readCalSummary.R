b06960cf156b4e52c656ffae2bceb1eddd573661
Kevin Stierhoff <kevin.stierhoff@noaa.gov>
2026-04-30 16:13:21 -0700

    Minor updates

--- Code/readCalSummary.R
+++ Code/readCalSummary.R
@@ -8,19 +8,23 @@ cal <- read_sheet("https://docs.google.com/spreadsheets/d/1wE-CLEnHa_wNspp9QBUuo
 cal.setup <- read_sheet("https://docs.google.com/spreadsheets/d/1wE-CLEnHa_wNspp9QBUuoT-ItvOiePUIR-9ciDcXANE/edit?gid=487864540#gid=487864540",
                   sheet = "setup-KLS")
 
+# Create labels for plot panels
 cal.setup.summ <- cal.setup %>% 
   group_by(group) %>% 
   summarise(g1 = min(setup), 
             g2 = max(setup)) %>% 
   mutate(label = paste(g1,"vs.",g2))
 
+# Add info to cal data
 cal <- cal %>% 
   left_join(select(cal.setup, setup, group, sphere_num)) %>% 
   left_join(select(cal.setup.summ, group, label))
 
+# Set plotting theme
 theme_set(theme_bw())
 
-# Plot gain
+# Plot comparisons
+## Gain
 cal.gain <- ggplot(cal, aes(factor(frequency), gain, group = setup, colour = sphere_num)) + 
   geom_line(show.legend = TRUE) + geom_point(show.legend = TRUE) +
   facet_wrap(~label) +
@@ -29,7 +33,7 @@ cal.gain <- ggplot(cal, aes(factor(frequency), gain, group = setup, colour = sph
        x = "Freqency (kHz)",
        y = "Gain (dB)")
 
-# Plot gain
+# Gain adjustment
 cal.gain.adj <- ggplot(cal, aes(factor(frequency), gain_adj, group = setup, colour = sphere_num)) + 
   geom_line(show.legend = TRUE) + geom_point(show.legend = TRUE) +
   facet_wrap(~label) +
